3cc0e83a824db090d1bcadca064b1db04040850c
Jose Dapena <j.dapena@lgepartner.com>
2021-03-10 07:58:20 +0000

    [op][n_upsable][webrtc] Enable webcam audio source for webRTC
    :Release Notes:
    Enable webcam audio source for webRTC
    
    :Detailed Notes:
    In webOS webRTC, the only source of audio should be webcam.
    This CL force to use web cam audio source for media capture and
    Blacklist LGSIC audio device. since LGSIC audio device is selected
    by default, Failed to get access to local media error is observed.
    With this changes Audio source is getting selected from USB device.
    
    :Testing Performed:
    1. Connect a webcam having mic to the device (auto/ose)
    2. Launch https://meet.jit.si and host a conference
    3. Join the conference from a laptop/mobile
    4. Start speaking on webcam mic connected to the device
    5. Ensure that the speech is audible on laptop/mobile speaker
    6. Check the same using skype web also.
    
    :QA Notes:
    Test-cases: GT-43085
    
    :Issues Addressed:
    [PLAT-137937] Enable webcam audio source for webRTC
    
    Change-Id: I70d4eff835c5e7042b8751f749e6aed95146dbc4
    Originally-Reviewed-on: http://gpro.lge.com/283445
    Originally-Reviewed-on: http://gpro.lge.com/305124
    Reviewed-on: http://gpro.lge.com/c/webos-pro/chromium87/+/306102
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Karthick Sundaram <karthick.sundaram@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Karthick Sundaram <karthick.sundaram@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium87/+/308754
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/content/browser/renderer_host/media/media_devices_manager.cc
+++ src/content/browser/renderer_host/media/media_devices_manager.cc
@@ -49,6 +49,11 @@ namespace content {
 
 namespace {
 
+#if defined(OS_WEBOS)
+const char kWebOSDefaultAudioDeviceId[] = "sysdefault:CARD=";
+const char kWebOSBlacklistedAudioDeviceId[] = "sysdefault:CARD=LGSIC";
+#endif
+
 // Resolutions used if the source doesn't support capability enumeration.
 struct {
   uint16_t width;
@@ -911,6 +916,20 @@ void MediaDevicesManager::AudioDevicesEnumerated(
 
   blink::WebMediaDeviceInfoArray snapshot;
   for (const media::AudioDeviceDescription& description : device_descriptions) {
+#if defined(OS_WEBOS)
+    if (type == blink::MEDIA_DEVICE_TYPE_AUDIO_INPUT &&
+        description.unique_id.compare(
+            0, base::size(kWebOSDefaultAudioDeviceId) - 1,
+            kWebOSDefaultAudioDeviceId) != 0) {
+      continue;
+    }
+    if (type == blink::MEDIA_DEVICE_TYPE_AUDIO_INPUT &&
+        description.unique_id.compare(
+            0, base::size(kWebOSBlacklistedAudioDeviceId) - 1,
+            kWebOSBlacklistedAudioDeviceId) == 0) {
+      continue;
+    }
+#endif
     snapshot.emplace_back(description.unique_id, description.device_name,
                           description.group_id,
                           media::VideoCaptureControlSupport(),
